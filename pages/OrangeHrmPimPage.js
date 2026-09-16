4f207b57e91f226eca1c947301b825cc7725e659
Anjali <anjalpandey1123@gmail.com>
2026-09-15 22:54:09 -0500

    orangehrm test case fail fixation

--- pages/OrangeHrmPimPage.js
+++ pages/OrangeHrmPimPage.js
@@ -3,10 +3,11 @@ const { BasePage } = require('./BasePage');
 class OrangeHrmPimPage extends BasePage {
   constructor(page) {
     super(page);
-    this.addEmployeeButton = page.getByRole('button', { name: 'Add', exact: true });
+    this.addEmployeeButton = page.getByRole('button', { name: 'Add'});
     this.firstNameInput = page.locator('input[name="firstName"]');
     this.middleNameInput = page.locator('input[name="middleName"]');
     this.lastNameInput = page.locator('input[name="lastName"]');
+    this.employeeIdInput = page.locator('input.oxd-input').nth(3);
     this.saveButton = page.getByRole('button', { name: 'Save', exact: true });
     this.employeeNameInput = page.locator('input[placeholder="Type for hints..."]').first();
     this.searchButton = page.getByRole('button', { name: 'Search', exact: true });
@@ -14,10 +15,11 @@ class OrangeHrmPimPage extends BasePage {
     this.employeeListHeading = page.getByRole('heading', { name: 'Employee Information' });
   }
 
-  async addEmployee(firstName, lastName) {
+  async addEmployee(firstName, lastName, employeeId = `PW${Date.now()}`) {
     await this.addEmployeeButton.click();
     await this.firstNameInput.fill(firstName);
     await this.lastNameInput.fill(lastName);
+    await this.employeeIdInput.fill(employeeId);
     await this.saveButton.click();
   }
 

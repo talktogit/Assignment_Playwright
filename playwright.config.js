2d2737bce704f06332b507e953f4e86b67382bf9
Anjali <anjalpandey1123@gmail.com>
2026-09-17 15:22:53 -0500

    screesnhot functionality

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
@@ -22,6 +22,7 @@ module.exports = defineConfig({
   use: {
     trace: 'on-first-retry',
     headless: true,
+    screenshot: 'on'
   },
 
   projects: [

90eaf9b04b5792e8ad5725b24bd8c05907cf5e09
Anjali <anjalpandey1123@gmail.com>
2026-09-17 22:14:19 -0500

    remove unused file

--- verify-orangehrm.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Set-Location 'C:\Users\DELL\OneDrive\Documents\Assignment_Playwright'
-& 'C:\Program Files\nodejs\npx.cmd' playwright test tests/orangeHrm.spec.js --reporter=line --workers=1
-exit $LASTEXITCODE
